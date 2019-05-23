4d8972ec713aebc61ee9baa56b187dd8cc631164
codemanpete <rhoxwarrior@gmail.com>
2019-05-22 22:09:40 -0500

    creates passport.js

---
+++ src/config/passport.js
@@ -0,0 +1,27 @@
+import {
+    JwtStrategy,
+    ExtractJwt
+} from 'passport-jwt';
+import mongoose from 'mongoose';
+import keys from './keys';
+
+const User = mongoose.model("users");
+const opts = {
+    secretOrKey: keys.secretOrKey,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+};
+
+export default function (passport) {
+    passport.use(
+        new JwtStrategy(opts, (jwtPayload, done) => {
+            User.findById(jwtPayload.id)
+                .then(user => {
+                    if (user) {
+                        return done(null, user);
+                    }
+                    return done(null, false);
+                })
+                .catch(err => console.log(err));
+        })
+    );
+}
