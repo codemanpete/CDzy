6355313776b591a79ddd9a7ee355574c735d1cc1
codemanpete <rhoxwarrior@gmail.com>
2019-05-28 22:51:55 -0500

    Creates register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {
+    Component
+} from 'react';
+import {
+    Link
+} from 'react-router-dom';
 
 class Register extends Component {
     constructor() {
         super();
         this.state = {
-            name: "",
+            username: "",
             email: "",
             password1: "",
             password2: "",
             errors: {}
         };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onChange = e => {
-        this.setState({ [e.target.id]: e.target.value });
-    };
+    onChange(e) {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
 
-    onSubmit = e => {
+    onSubmit(e) {
         e.preventDefault();
 
         const newUser = {
-            name: this.state.name,
+            username: this.state.username,
             email: this.state.email,
             password1: this.state.password1,
             password2: this.state.password2
@@ -29,9 +37,97 @@ class Register extends Component {
 
         console.log("Submitting user: ");
         console.log(newUser);
-    };
+    }
 
-
+    render() {
+        const { errors } = this.state;
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">
+                                keyboard_backspace
+                            </i>
+                            Back to home
+                        </Link>
+                        <div className="col s12" style={{
+                            paddingLeft: "11.250px"
+                        }}>
+                            <h4>
+                                <b>Register</b> below
+                            </h4>
+                            <p className="grey-text text-darken-1">
+                                Already have an account?
+                                <Link to="/login">Login</Link>
+                            </p>
+                        </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.username}
+                                    error={errors.username}
+                                    id="username"
+                                    type="text"
+                                />
+                                <label htmlFor="username">Username</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.email}
+                                    error={errors.email}
+                                    id="email"
+                                    type="email"
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.password1}
+                                    error={errors.password1}
+                                    id="password1"
+                                    type="password"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.password2}
+                                    error={errors.password2}
+                                    id="password2"
+                                    type="password"
+                                />
+                                <label htmlFor="password2">
+                                    Confirm Password
+                                </label>
+                            </div>
+                            <div className="col s12" style={{
+                                paddingLeft: "11.250px"
+                            }}>
+                                <button
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-large waves-effect 
+                                        waves-light hoverable blue accent-3"
+                                >
+                                    Sign up
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Register;
