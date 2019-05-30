fe3ca3eb5610856da1d6549f6d8af5fa4022dab1
codemanpete <rhoxwarrior@gmail.com>
2019-05-29 21:53:28 -0500

    Shortens long lines

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -34,10 +34,15 @@ class Login extends Component {
                 <div style={{ marginTop: "4rem" }} className="row">
                     <div className="col s8 offset-s2">
                         <Link to="/" className="btn-flat waves-effect">
-                            <i className="material-icons left">keyboard_backspace</i>
+                            <i className="material-icons left">
+                                keyboard_backspace
+                            </i>
                             Back to home
                         </Link>
-                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                        <div className="col s12"
+                            style={{
+                                paddingLeft: "11.250px"
+                            }}>
                             <h4>
                                 <b>Login</b> below
                             </h4>
@@ -68,7 +73,9 @@ class Login extends Component {
                                 />
                                 <label htmlFor="password">Password</label>
                             </div>
-                            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <div className="col s12" style={{
+                                paddingLeft: "11.250px"
+                            }}>
                                 <button
                                     type="submit"
                                     style={{
@@ -77,7 +84,8 @@ class Login extends Component {
                                         letterSpacing: "1.5px",
                                         marginTop: "1rem"
                                     }}
-                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                    className="btn btn-large waves-effect 
+                                        waves-light hoverable blue accent-3"
                                 >
                                     Login
                                 </button>
