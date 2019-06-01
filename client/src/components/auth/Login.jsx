99759998d19b3ba896bfc6345535193b431481a1
codemanpete <rhoxwarrior@gmail.com>
2019-05-31 20:48:24 -0500

    Connects Login component to redux and actions

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
+import classnames from 'classnames';
 
 class Login extends Component {
     constructor() {
@@ -11,6 +15,17 @@ class Login extends Component {
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
     }
 
     onChange(e) {
@@ -25,6 +40,7 @@ class Login extends Component {
         };
         console.log("logging in with data: ");
         console.log(userData);
+        this.props.loginUser(userData);
     }
 
     render() {
@@ -60,8 +76,16 @@ class Login extends Component {
                                     errors={errors.email}
                                     id="email"
                                     type="email"
+                                    className={classnames("", {
+                                        invalid: errors.email || 
+                                            errors.emailnotfound
+                                    })}
                                 />
                                 <label htmlFor="email">Email</label>
+                                <span className="red-text">
+                                    {errors.email}
+                                    {errors.emailnotfound}
+                                </span>
                             </div>
                             <div className="input-field col s12">
                                 <input
@@ -70,8 +94,16 @@ class Login extends Component {
                                     errors={errors.password}
                                     id="password"
                                     type="password"
+                                    className={classnames("", {
+                                        invalid: errors.password || 
+                                            errors.passwordincorrect
+                                    })}
                                 />
                                 <label htmlFor="password">Password</label>
+                                <span className="red-text">
+                                    {errors.password}
+                                    {errors.passwordincorrect}
+                                </span>
                             </div>
                             <div className="col s12" style={{
                                 paddingLeft: "11.250px"
@@ -98,4 +130,18 @@ class Login extends Component {
     }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = ({ auth, errors}) => ({
+    auth,
+    errors
+});
+
+export default connect(
+    mapStateToProps,
+    { loginUser }
+)(withRouter(Login));
