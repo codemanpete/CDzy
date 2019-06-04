dafc0f64deb69bc074931dcfa652f168eea4c547
codemanpete <rhoxwarrior@gmail.com>
2019-06-03 19:52:13 -0500

    Adds styling for dashboard page

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -12,14 +12,16 @@ class Dashboard extends Component {
     render() {
         const { user } = this.props.auth;
         return (
-            <div>
-                <div>
-                    <div>
+            <div style={{
+                height: "75vh"
+            }} className="container valign-wrapper">
+                <div className="row">
+                    <div className="col s12 center-align">
                         <h4>
-                            <b>Hey there,</b>
+                            <b>Hey there,</b>&nbsp;
                             {user.username}
-                            <p>
-                                You are logged into a full-stack{" "}
+                            <p className="flow-text grey-text text-darken-1">
+                                You are logged into a full-stack&nbsp;
                                 <span style={{
                                     fontFamily: "monospace"
                                 }}>MERN</span>
@@ -27,7 +29,15 @@ class Dashboard extends Component {
                             </p>
                         </h4>
                         <button
+                            style={{
+                                width: "150px",
+                                borderRadius: "3px",
+                                letterSpacing: "1.5px",
+                                marginTop: "1rem"
+                            }}
                             onClick={this.onLogoutClick.bind(this)}
+                            className="btn btn-large waves-effect waves-light
+                                hoverable blue accent-3"
                         >
                             Logout
                         </button>
