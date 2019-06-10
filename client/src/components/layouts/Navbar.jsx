e096fd60335dc54204edd3c075b83d3d8d805857
codemanpete <rhoxwarrior@gmail.com>
2019-06-10 18:34:53 -0500

    Updates styling and links for navbar

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -6,16 +6,30 @@ class Navbar extends Component {
         return (
             <div className="navbar-fixed">
                 <nav className="z-depth-0">
-                    <div className="nav-wrapper white">
+                    <div className="nav-wrapper indigo accent-3">
                         <Link
                             to="/"
                             style={{
                                 fontFamily: "monospace"
                             }}
-                            className="col s5 brand-logo center black-text">
+                            className="brand-logo">
                             <i className="material-icons">code</i>
                             CDzy
                         </Link>
+                        <ul className="right">
+                            <li>
+                                <Link to="/dashboard">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/accounts">Accounts</Link>
+                            </li>
+                            <li>
+                                <Link to="/browse">Browse</Link>
+                            </li>
+                            <li>
+                                <Link to="#">My account<i class="material-icons right">arrow_drop_down</i></Link>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
